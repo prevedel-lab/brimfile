9ec6a08629a009a28e9919e5c9ad6a71032c3a4a
Carlo Bevilacqua <carlo.bevilacqua@embl.de>
2025-08-29 19:46:58 +0200

    Added `loadZarrFile` to the global scope`

--- src/js/zarr_wrapper.js
+++ src/js/zarr_wrapper.js
@@ -1,5 +1,4 @@
 import { ZarrFile, init_file } from './zarr_file.js';
-//import { loadPyodide } from "https://cdn.jsdelivr.net/pyodide/v0.28.2/full/pyodide.mjs";
 
 // Loads the Zarr and create a bls_file in the globals of pyodide
 function loadZarrFile(file) {
@@ -16,29 +15,5 @@ function loadZarrFile(file) {
     `, { locals });
     return true;
 }
-
-//TEST
-/*
-async function init_pyodide() {
-    self.pyodide = await loadPyodide();
-    await pyodide.loadPackage("micropip");
-    await pyodide.loadPackage("numpy")
-    await pyodide.loadPackage("./brimfile-1.1.3-py2.py3-none-any.whl")
-}
-init_pyodide().then (
-    () => {
-        loadZarrFile('https://storage.googleapis.com/brim-example-files/zebrafish_eye_confocal.brim.zarr');
-        pyodide.runPythonAsync(`
-            from js import console
-            dg = bls_file.list_data_groups(retrieve_custom_name=True)
-            console.log(str(dg[0]))
-
-            d = bls_file.get_data()
-
-            # get the metadata 
-            md = d.get_metadata()
-            all_metadata = md.all_to_dict()
-            console.log(str(all_metadata['Experiment']))`)
-    }
-)
-*/
+//make sure loadZarrFile is in the global scope
+self.loadZarrFile = loadZarrFile;
