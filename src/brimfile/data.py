Fixed average

src/brimfile/data.py
@@ -672,7 +672,7 @@ class Data:
                     pts += (Data.AnalysisResults.PeakType.AntiStokes,)
                 #average getting the units of the first peak
                 res[qt.name][Data.AnalysisResults.PeakType.average.name] = Metadata.Item(
-                    np.mean([res[qt.name][pt.name].value for pt in pts]), 
+                    np.mean([np.abs(res[qt.name][pt.name].value) for pt in pts]), 
                     res[qt.name][pts[0].name].units
                     )
                 if not all(res[qt.name][pt.name].units == res[qt.name][pts[0].name].units for pt in pts):